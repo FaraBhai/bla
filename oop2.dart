a6d37948afb4e2c6b19b0f3250b5a7ce21c73955
Fara Bhai <farashikder09@gmail.com>
2026-01-25 23:58:33 +0600

    added new model

--- oop2.dart
+++ oop2.dart
@@ -13,4 +13,7 @@ void main(){
 
   iphone13.setDevicePrice=1255;
   print(iphone13.currentPrice);
+    mobile s24ultra=mobile(m: 's24ultra', year: '2020', price: 123445.0);
+
+  gshop.addNewMovile(s24ultra);
 }
